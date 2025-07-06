baed9d0f0d50fc05854f02463447d4f204d16688
Z3roKwq <z3rokwq@gmail.com>
2025-07-06 14:06:10 +0300

    pissarro: Migrate TARGET_INIT_VENDOR_LIB to soong_config_set

--- device.mk
+++ device.mk
@@ -391,6 +391,9 @@ PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
     android.hardware.usb.gadget-service.mediatek
 
+# Vendor Init
+$(call soong_config_set, libinit, vendor_init_lib, //$(LOCAL_PATH):init_xiaomi_pissarro)
+
 # Vibrator
 $(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
 

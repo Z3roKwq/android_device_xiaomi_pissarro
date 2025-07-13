e4b64f305dc1a952a22e95ee70fa19d782ba405c
Z3roKwq <z3rokwq@gmail.com>
2025-07-13 22:15:35 +0300

    Revert "pissarro: Switch to common USB service"
    This reverts commit d90cac739fa8074d3c49fc885d845f8e382e1005.

--- device.mk
+++ device.mk
@@ -389,8 +389,8 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.mediatek \
-    android.hardware.usb.gadget-service.mediatek
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.1.vendor
 
 # Vendor Init
 $(call soong_config_set, libinit, vendor_init_lib, //$(LOCAL_PATH):init_xiaomi_pissarro)

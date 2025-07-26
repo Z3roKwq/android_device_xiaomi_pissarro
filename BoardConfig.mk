0e06c85fc6246fa869916ffe1b438f95cd10e74a
Z3roKwq <z3rokwq@gmail.com>
2025-07-26 12:35:06 +0300

    pissarro: Enable cpuidle governor switching
    * Enable cpuidle_sysfs_switch kernel parameter to allow runtime
      switching between cpuidle governors. By default, the system
      uses 'menu' governor with read-only sysfs interface, preventing
      changes to alternative governors like TEO. This flag makes the
      cpuidle configuration writable via sysfs.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x07c08000
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.tee_type=2
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.tee_type=2 cpuidle_sysfs_switch
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_MKBOOTIMG_ARGS := --base $(BOARD_KERNEL_BASE)

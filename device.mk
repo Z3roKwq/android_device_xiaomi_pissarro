66d31d38adcce7adeb756c73e88de89473254850
Z3roKwq <z3rokwq@gmail.com>
2025-08-01 18:05:01 +0300

    pissarro: Switch to MediaTek sensors subhal wrapper

--- device.mk
+++ device.mk
@@ -284,6 +284,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-subhal-impl-1.0:64 \
     android.hardware.sensors-service.xiaomi-multihal
 
 PRODUCT_COPY_FILES += \
